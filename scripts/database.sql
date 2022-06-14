231c4befd01b2f1ce674f5a1548c74c29baa5b59
luisgs7 <luisgsilva270@gmail.com>
2022-06-14 16:32:26 -0300

    create dabase, remove

---
+++ scripts/database.sql
@@ -0,0 +1,8 @@
+# criar um banco de dados
+CREATE DATABASE banco_teste;
+
+# Adicionar todas as permissões de um bando de dados para um usuário
+GRANT ALL ON DATABASE banco_teste TO fabio;
+
+# Excluir um banco de dados
+DROP DATABASE banco_teste;
